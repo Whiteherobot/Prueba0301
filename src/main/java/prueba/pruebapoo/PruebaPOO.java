09a95dd098aaa2cfb8579979f57b2f6058bf252a
mlata <mlata@Michi>
2022-06-17 08:04:19 -0500

    implementation in main system

--- src/main/java/prueba/pruebapoo/PruebaPOO.java
+++ src/main/java/prueba/pruebapoo/PruebaPOO.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package prueba.pruebapoo;
-
-/**
- *
- * @author mlata
- */
+//@michi
+import vistaPartido.partidoVista;
 public class PruebaPOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        var v1 = new partidoVista();
+        v1.setVisible(true);
     }
 }
